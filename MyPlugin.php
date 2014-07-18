c5ba440ae9888ad1d98bde49c858f990be6f8e5f
llpereiras <llpereiras@gmail.com>
2014-07-18 10:43:25 -0300

    Create MyPlugin.php

---
+++ MyPlugin.php
@@ -0,0 +1,34 @@
+<?php
+namespace My\Controller\Plugin;
+
+use Zend\Mvc\Controller\Plugin\AbstractPlugin;
+use Zend\ServiceManager\ServiceLocatorAwareInterface;
+use Zend\ServiceManager\ServiceLocatorInterface;	
+
+class MyPlugin extends AbstractPlugin implements ServiceLocatorAwareInterface{
+
+	private $services;
+	
+	public function setServiceLocator(ServiceLocatorInterface $serviceLocator)
+	{
+		$this->services = $serviceLocator;
+	}
+
+	public function getServiceLocator()
+	{
+		return $this->services;
+	}
+
+	public function getModel()
+	{
+		$service = $this->getServiceLocator();
+		$adapter = $service->getServiceLocator()->get('Zend\Db\Adapter\Adapter');
+		$class = '\Application\Model\\QueueModel';
+		return new $class($adapter, $service);
+	}
+
+	protected function getViewHelper($viewHelper)
+	{
+		return $this->getServiceLocator()->get('viewhelpermanager')->get($viewHelper);
+	}
+}
